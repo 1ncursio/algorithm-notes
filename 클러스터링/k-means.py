983c967e95dd0058bc5eaf4917cff05d6d445c58
1ncursio <ckswn1323@naver.com>
2022-06-07 11:00:14 +0900

    [Modify] k-means 구현 완료

--- 클러스터링/k-means.py
+++ 클러스터링/k-means.py
@@ -8,9 +8,10 @@ from typing import List, Tuple
 
 Coord = Tuple[int, int]
 Matrix = List[List[bool]]
+Cluster = List[List[int]]
 
 
-def k_means(matrix: Matrix, k: int) -> List[List[int]]:
+def k_means(matrix: Matrix, k: int) -> Tuple[Cluster, List[Coord], int]:
     """
     k-means clustering algorithm
     1. 중심점을 k개 랜덤하게 생성한다.
@@ -24,17 +25,26 @@ def k_means(matrix: Matrix, k: int) -> List[List[int]]:
     cur_centroids = [(int(random() * size), int(random() * size)) for _ in range(k)]
     prev_centroids = []
 
+    # 총 시행 횟수
+    count = 0
+
+    """
+    중심점이 갱신되는 동안:
+        Matrix의 각 노드에 대해:
+            현재 노드와의 거리가 최소인 중심점을 찾아서, cluster 행렬을 갱신한다.
+        각 클러스터에 포함한 원소들의 중앙값으로 중심점을 갱신한다.
+    """
     while cur_centroids != prev_centroids:
         for i in range(size):
             for j in range(size):
                 if matrix[i][j]:
                     dists = get_distances_to_node((i, j), cur_centroids)
-                    min_dist = min(dists)
-                    cluster[i][j] = dists.index(min_dist) + 1
+                    cluster[i][j] = dists.index(min(dists)) + 1
 
         prev_centroids = cur_centroids[:]
+
         for K in range(k):
-            count = 0
+            count_k = 0
             acc_y = 0
             acc_x = 0
 
@@ -43,16 +53,19 @@ def k_means(matrix: Matrix, k: int) -> List[List[int]]:
                     if cluster[i][j] == K + 1:
                         acc_y += i
                         acc_x += j
-                        count += 1
+                        count_k += 1
 
-            # acc_y //= count
-            # acc_x //= count
+            acc_y //= count_k
+            acc_x //= count_k
 
             cur_centroids[K] = (acc_y, acc_x)
 
+        count += 1
+
+        print(f"----- {count}번째 시행 -----")
         _print_cluster(cluster)
 
-    print(cur_centroids)
+    return cluster, cur_centroids, count
 
 
 def init_matrix(size: int, points: int) -> Matrix:
@@ -68,12 +81,11 @@ def init_matrix(size: int, points: int) -> Matrix:
 
 
 def get_distances_to_node(from_coord: Coord, nodes: List[Coord]) -> List[int]:
-    dists = []
+    dists: List[int] = []
 
     for node in nodes:
         dist = int(get_euclidean_distance(from_coord, node))
         dists.append(dist)
-    print(dists)
 
     return dists
 
@@ -93,10 +105,12 @@ def _print_matrix(matrix: Matrix) -> None:
     print()
 
 
-def _print_cluster(matrix: List[List[int]]) -> None:
-    for row in matrix:
+def _print_cluster(cluster: Cluster) -> None:
+    clusters = ["★", "☆", "○", "●", "◎", "◇", "◆", "□", "■"]
+
+    for row in cluster:
         for cell in row:
-            print("." if cell == 0 else cell, end=" ")
+            print("." if cell == 0 else clusters[cell - 1], end=" ")
         print()
     print()
 
@@ -104,7 +118,10 @@ def _print_cluster(matrix: List[List[int]]) -> None:
 if __name__ == "__main__":
     size = int(input("행렬의 크기를 입력하세요(기본 20): ") or 20)
     points = int(input("노드의 개수를 입력하세요(기본 80): ") or 80)
+    k = int(input("클러스터의 개수를 입력하세요(기본 3, 최대 9): ") or 3)
+
     matrix = init_matrix(size, points)
+    print("초기 행렬:")
     _print_matrix(matrix)
-    k = 3
-    k_means(matrix, k)
+    cluster, centroids, count = k_means(matrix, k)
+    print(f"총 시행 횟수: {count}")
